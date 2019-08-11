435f65c82bb10521395346dd6b6d903549137403
Aaron Pedro <aaron.jay.pedro@gmail.com>
2019-08-11 02:32:21 -0600

    add lookup code

--- misc.js
+++ misc.js
@@ -1,3 +1,7 @@
+const fdict = require('./features/dictionary');
+const edict = require('./edges/dictionary');
+const v     = require('voca');
+
 module.exports = {
     commands : () => {
         let str = `
@@ -11,5 +15,28 @@ features <feature name> - Get details on a specific feature
 listfeatures - Show usage of list features and the available categories
 listfeatures <feature category> - Show all features from a specific category`;
         return str;
+    },
+    lookup : (term) => {
+        let retVal = "";
+        formatted_text = v.lowerCase(term);
+        if(fdict.feature_dictionary[formatted_text])
+            retVal =  "**"
+            + fdict.feature_dictionary[formatted_text].name 
+            + "**\r" 
+            + "**Type: ** Feature\r"
+            + "**Category: **" + fdict.feature_dictionary[formatted_text].category 
+            +"\r"
+            + fdict.feature_dictionary[formatted_text].desc;
+        else if(edict.edge_dictionary[formatted_text])
+            retVal = "**"
+            + edict.edge_dictionary[formatted_text].name 
+            + "**\r" 
+            + "**Type: ** Edge\r"
+            + "**Category: **" + edict.edge_dictionary[formatted_text].category 
+            +"\r"
+            + edict.edge_dictionary[formatted_text].desc;
+        else
+            return "**Lookup failure: **" + term +" was not found.";
+        return retVal;
     }
 }
