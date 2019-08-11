544218d5d5354430ac2d922887b3890524b5d414
Aaron Pedro <aaron.jay.pedro@gmail.com>
2019-08-11 00:53:52 -0600

    Add commands for feature lookups in !commands

--- misc.js
+++ misc.js
@@ -2,9 +2,14 @@ module.exports = {
     commands : () => {
         let str = `
 commands - Show this list
+
 edges <edge name> - Get details on a specific edge
 listedges - Show usage of list edges and the available categories
-listedges <edge category> - Show all edges from a specific category`;
+listedges <edge category> - Show all edges from a specific category
+
+features <feature name> - Get details on a specific feature
+listfeatures - Show usage of list features and the available categories
+listfeatures <feature category> - Show all features from a specific category`;
         return str;
     }
 }
