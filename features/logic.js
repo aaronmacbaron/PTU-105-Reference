0602709000df6cded6f42f9c42944bb960993a36
Aaron Pedro <aaron.jay.pedro@gmail.com>
2019-08-11 00:55:07 -0600

    implement logic file for features

---
+++ features/logic.js
@@ -0,0 +1,49 @@
+const v = require('voca');
+var logger = require('winston');
+const fdict = require('./dictionary');
+
+
+module.exports = {
+    lookup : (feature) => {
+        formatted_feature = v.lowerCase(feature);
+        if(fdict.feature_dictionary[formatted_feature])
+            return  "**"
+            + fdict.feature_dictionary[formatted_feature].name 
+            + "**\r" 
+            + "**Category: **" + fdict.feature_dictionary[formatted_feature].category 
+            +"\r"
+            + fdict.feature_dictionary[formatted_feature].desc 
+        else
+        return fdict.error.non_existent_error;    
+    },
+    listCategories: () => {
+        let categories = [];
+        for (var entry in fdict.feature_dictionary) {
+            let category = fdict.feature_dictionary[entry].category;   
+            if(!categories.includes(category))
+                categories.push(category)
+        }
+        return `
+        **Usage:** !listfeatures <category name>
+**Example:** !listfeatures Combat
+Here is a list of the different categories:\r`+
+         categories.join(", ");
+    },
+    allByCategory : (featureCategory) => {
+        let list = [];
+        let formattedCategory = v.lowerCase(featureCategory);
+        for (var entry in fdict.feature_dictionary) {
+            if(v.lowerCase(fdict.feature_dictionary[entry].category) === formattedCategory)
+                list.push(fdict.feature_dictionary[entry].name);
+        }
+        list.sort(function(a, b) {
+            var textA = a.toUpperCase();
+            var textB = b.toUpperCase();
+            return (textA < textB) ? -1 : (textA > textB) ? 1 : 0;
+        });
+        return list.join("\r");
+    },
+    usage : () => {
+        return `**usage:** !features <feature name>\rexample: !features Defender`;
+    }
+}
